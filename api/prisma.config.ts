d20642bb33fdd29915f6036861a6b4dd9cda3cde
faiz140405 <cahmargablok51@gmail.com>
2025-12-10 21:39:45 +0700

    update dabase NOTE: jangan di rubah lagi apabila di rubah atau pembaruan jangan lupa npx migrate terlebih dahulu

--- api/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "prisma/config";
-
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-
-  env: {
-    DATABASE_URL: "postgresql://username:password@localhost:5432/manabucards?schema=public",
-  },
-});
