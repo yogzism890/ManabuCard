2d9cec6c6db98d685542d362417241e0fd6fd24b
yogzism890 <yogznovachrono@gmail.com>
2026-01-16 19:26:54 +0700

    mengganti logo di login

--- manabucard/app/auth/login.tsx
+++ manabucard/app/auth/login.tsx
@@ -52,7 +52,7 @@ export default function LoginScreen() {
           <View style={styles.logoWrapper}>
             <View style={styles.logoInner}>
               <Animated.Image
-                source={require("@/assets/images/manabulogo.png")}
+                source={require("@/assets/images/logo.png")}
                 style={styles.logo}
                 resizeMode="contain"
               />
